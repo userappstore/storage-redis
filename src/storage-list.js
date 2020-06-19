fe6bd2131e067ace765c897dfb0da122054e7ebe
benlowry <benlowry@outlook.com>
2020-06-19 13:13:47 -0600

    Prefix all lists

--- src/storage-list.js
+++ src/storage-list.js
@@ -19,7 +19,7 @@ module.exports = {
             Log.error('error adding item', error)
             return callback(error)
           }
-          return storage.client.lpush(path, itemid, (error) => {
+          return storage.client.lpush(`list/${path}`, itemid, (error) => {
             if (error) {
               Log.error('error adding item', error)
               return callback(error)
@@ -36,12 +36,12 @@ module.exports = {
           }
           const path = paths.shift()
           const itemid = items[path]
-          return storage.client.hsetnx(`list/${path}`, itemid, '1', (error) => {
+          return storage.client.hsetnx(`list/map/${path}`, itemid, '1', (error) => {
             if (error) {
               Log.error('error adding item', error)
               return callback(error)
             }
-            return storage.client.lpush(path, itemid, (error) => {
+            return storage.client.lpush(`list/${path}`, itemid, (error) => {
               if (error) {
                 Log.error('error adding many items', error)
                 return callback(new Error('unknown-error'))
@@ -53,7 +53,7 @@ module.exports = {
         return nextItem()
       }),
       count: util.promisify((path, callback) => {
-        return storage.client.llen(path, (error, result) => {
+        return storage.client.llen(`list/${path}`, (error, result) => {
           if (error) {
             Log.error('error adding item', error)
             return callback(new Error('unknown-error'))
@@ -62,7 +62,7 @@ module.exports = {
         })
       }),
       exists: util.promisify((path, itemid, callback) => {
-        return storage.client.hexists(`list/${path}`, itemid, (error, index) => {
+        return storage.client.hexists(`list/map/${path}`, itemid, (error, index) => {
           if (error) {
             Log.error('error adding many items', error)
             return callback(new Error('unknown-error'))
@@ -78,7 +78,7 @@ module.exports = {
         if (offset < 0) {
           throw new Error('invalid-offset')
         }
-        return storage.client.lrange(path, offset, offset + pageSize - 1, (error, results) => {
+        return storage.client.lrange(`list/${path}`, offset, offset + pageSize - 1, (error, results) => {
           if (error) {
             Log.error('error listing', error)
             return callback(new Error('unknown-error'))
@@ -87,7 +87,7 @@ module.exports = {
         })
       }),
       listAll: util.promisify((path, callback) => {
-        return storage.client.lrange(path, 0, -1, (error, results) => {
+        return storage.client.lrange(`list/${path}`, 0, -1, (error, results) => {
           if (error) {
             Log.error('error listing all', error)
             return callback(new Error('unknown-error'))
@@ -96,7 +96,7 @@ module.exports = {
         })
       }),
       remove: util.promisify((path, itemid, callback) => {
-        return storage.client.lrem(path, 1, itemid, (error) => {
+        return storage.client.lrem(`list/${path}`, 1, itemid, (error) => {
           if (error) {
             Log.error('error removing', error)
             return callback(new Error('unknown-error'))
